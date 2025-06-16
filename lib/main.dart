b22cf61b258516b3c3e7546e5b33ce63e3dbd24f
fxsym <fsyamsudin24@gmail.com>
2025-06-16 23:53:32 +0700

    False chekced mode banner

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Encrypt App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.deepPurple),
       initialRoute: '/login',
       routes: {
